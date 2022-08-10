269a0d276036337ee9da42ec7f266dd981403146
mcmarius <mcmarius@users.noreply.github.com>
2022-08-10 15:33:36 +0300

    Remove leaks

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
 int main() {
-    int v[2];
-    v[10] = 4;
-    std::cout << v[11] << "\n";
-    int *q = new int[12];
-    std::cout << q[4] << "\n";
     std::cout << "Hello, world!\n";
     return 0;
 }
