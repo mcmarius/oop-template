584a416863d49ca7e7adb3f8a9d5b8e0ca5d778f
mcmarius <mcmarius@users.noreply.github.com>
2022-08-10 10:03:37 +0300

    Add errors and leaks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+    int v[2];
+    v[10] = 4;
     std::cout << "Hello, world!\n";
+    std::cout << v[11] << "\n";
+    int *q = new int[12];
+    std::cout << q[4] << "\n";
     return 0;
 }
