a4de53bc9bb8054a2df682655ad726519414b29e
mcmarius <23401453+mcmarius@users.noreply.github.com>
2022-08-20 00:44:49 +0300

    Adăugat SFML
    - Shallow clone, ignore errors
    - Try to install less dependencies on Linux
    - Try 2.6.x branch
    
    Eliminare nume fereastră hardcodat în scripts
    
    Run xvfb-run directly
    
    Remove job running MSan
    
    We do not run MSan with SFML, so this job is not needed at all
    because we now run cppcheck in a different job.
    
    Install linux deps due to cmake config
    
    Add missing external dep
    
    This might be preinstalled on GitHub Actions VMs, but it
    simplifies local setup.
    
    Make it compile on gcc-12
    
    Use ninja-build
    
    Skip linking sanitizers on gcc
    
    Disable C++20 modules explicitly for MSVC
    
    Skip MSVC modules
    
    Just because MSVC insists on compiling modules
    despite being explicitly disabled
    
    Disable building STL modules on MSVC
    
    Add workaround source
    
    Comment explicit C++23
    
    For now, we set it globally
    
    Patch pentru a folosi SFML cu threads
    
    CMake build flag as option, comment unused step
    
    Link deps privately
    
    Copy all missing framework dylibs
    
    Copy OpenAL DLL on Windows
    
    SFML requires C runtime linked against MSVCRT (for 2.6.1).
    GitHub decided to switch to UCRT based builds of MinGW
    See https://github.com/actions/runner-images/discussions/8372
    and https://github.com/SFML/SFML/issues/2700
    
    Add missing shell param, fix label
    
    Fix deprecation
    
    Bump SFML to 2.6.1, refactor out quirks
    
    Instrucțiuni pentru compilare pe Linux
    
    Remove env_fixes.h
    
    Default variable values to run scripts locally
    
    Valgrind suppressions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <array>
+#include <chrono>
+#include <thread>
+
+#include <SFML/Graphics.hpp>
+
+//////////////////////////////////////////////////////////////////////
+/// This class is used to test that the memory leak checks work as expected even when using a GUI
+class SomeClass {
+public:
+    explicit SomeClass(int) {}
+};
+
+SomeClass *getC() {
+    return new SomeClass{2};
+}
+//////////////////////////////////////////////////////////////////////
+
 
 int main() {
+    ///
     std::cout << "Hello, world!\n";
     std::array<int, 100> v{};
     int nr;
@@ -46,5 +64,54 @@ int main() {
     ///     fis >> v2[i];
     ///
     ///////////////////////////////////////////////////////////////////////////
+
+    SomeClass *c = getC();
+    std::cout << c << "\n";
+    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
+
+    sf::RenderWindow window;
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
+    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
+    /// This is needed so we do not burn the GPU                            ///
+    window.setVerticalSyncEnabled(true);                                    ///
+    /// window.setFramerateLimit(60);                                       ///
+    ///////////////////////////////////////////////////////////////////////////
+
+    while(window.isOpen()) {
+        bool shouldExit = false;
+        sf::Event e{};
+        while(window.pollEvent(e)) {
+            switch(e.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::Resized:
+                std::cout << "New width: " << window.getSize().x << '\n'
+                          << "New height: " << window.getSize().y << '\n';
+                break;
+            case sf::Event::KeyPressed:
+                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
+                if(e.key.code == sf::Keyboard::Escape)
+                    shouldExit = true;
+                break;
+            default:
+                break;
+            }
+        }
+        if(shouldExit) {
+            window.close();
+            break;
+        }
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(300ms);
+
+        window.clear();
+        window.display();
+    }
     return 0;
 }
