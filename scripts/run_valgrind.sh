1066dff58188a5c4f65304194c61f26f8740238b
mcmarius <23401453+mcmarius@users.noreply.github.com>
2022-08-20 01:01:54 +0300

    More typos

---
+++ scripts/run_valgrind.sh
@@ -0,0 +1,7 @@
+cat tastatura.txt | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./artifacts/bin/"${EXECUTABLE_NAME}" &
+sleep 10 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Y &&
+sleep 1 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Z &&
+sleep 2 && xdotool search --name "My Window" key Alt+F4
+
