3530d6d06aaa34d5dc4986cc80f2bf8f1d56b688
mcmarius <23401453+mcmarius@users.noreply.github.com>
2022-08-20 01:01:54 +0300

    More typos

---
+++ scripts/run_sanitizers.sh
@@ -0,0 +1,6 @@
+cat tastatura.txt | ./artifacts/bin/"${EXECUTABLE_NAME}" &
+sleep 3 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Y &&
+sleep 1 && xdotool search --name "My Window" key X &&
+sleep 1 && xdotool search --name "My Window" key Z &&
+sleep 2 && xdotool search --name "My Window" key Alt+F4
