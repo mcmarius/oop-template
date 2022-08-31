b0a6051fe1ce85cb9b958aae4cadcc7863ea5f2f
mcmarius <23401453+mcmarius@users.noreply.github.com>
2022-08-31 14:13:35 +0300

    Update scripts after rebase

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,4 +1,4 @@
-cat tastatura.txt | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./artifacts/bin/"${EXECUTABLE_NAME}" &
+cat "${INPUT_FILENAME}" | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./bin/"${EXECUTABLE_NAME}" &
 sleep 10 && xdotool search --name "My Window" key X &&
 sleep 1 && xdotool search --name "My Window" key Y &&
 sleep 1 && xdotool search --name "My Window" key X &&
