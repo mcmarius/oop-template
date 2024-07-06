739333e6118351060c1cdb35afdc69ea3be2eee5
mcmarius <23401453+mcmarius@users.noreply.github.com>
2024-07-06 21:12:06 +0300

    Script-uri separate de CMake

---
+++ cmake/CompilerFlags.cmake
@@ -0,0 +1,27 @@
+# target definitions
+
+if(GITHUB_ACTIONS)
+    message("NOTE: GITHUB_ACTIONS defined")
+    target_compile_definitions(${PROJECT_NAME} PRIVATE GITHUB_ACTIONS)
+endif()
+
+###############################################################################
+
+if(WARNINGS_AS_ERRORS)
+    set_property(TARGET ${PROJECT_NAME} PROPERTY COMPILE_WARNING_AS_ERROR ON)
+endif()
+
+# custom compiler flags
+message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
+if(MSVC)
+    target_compile_options(${PROJECT_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0)
+else()
+    target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -pedantic)
+endif()
+
+###############################################################################
+
+# sanitizers
+include(cmake/CustomStdlibAndSanitizers.cmake)
+
+set_custom_stdlib_and_sanitizers(${PROJECT_NAME} true)
