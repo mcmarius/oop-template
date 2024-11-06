4de64f5c1cacf93dc8f5d2a7f578feda7a3c3bd1
mcmarius <23401453+mcmarius@users.noreply.github.com>
2024-11-06 02:35:56 +0200

    Build cpr statically on Linux
    Remove MSan check due to missing support by cpr

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -24,4 +24,9 @@ endif()
 # sanitizers
 include(cmake/CustomStdlibAndSanitizers.cmake)
 
+if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+else()
+    set_custom_stdlib_and_sanitizers(cpr false)
+endif()
+
 set_custom_stdlib_and_sanitizers(${MAIN_EXECUTABLE_NAME} true)
