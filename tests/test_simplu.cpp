acf4a954022f861fa4b1faac5950fd136e1b8ce6
Duta-Sebastian <sebiduta2004@gmail.com>
2024-11-12 00:23:39 +0200

    Disable modules for boost-ut + ignoram cppcheck pe test_simplu

--- tests/test_simplu.cpp
+++ tests/test_simplu.cpp
@@ -1,3 +1,4 @@
+#define BOOST_UT_DISABLE_MODULE
 #include <ut.hpp>
 
 int sum(const int a, const int b) {
@@ -7,14 +8,17 @@ int sum(const int a, const int b) {
 int main() {
     using namespace boost::ut;
 
+    // cppcheck-suppress knownConditionTrueFalse
     "sum of positive numbers"_test = [] {
         expect(sum(2, 3) == 5_i);
     };
 
+    // cppcheck-suppress knownConditionTrueFalse
     "sum with zero"_test = [] {
         expect(sum(5, 0) == 5_i);
     };
 
+    // cppcheck-suppress knownConditionTrueFalse
     "sum of negative numbers"_test = [] {
         expect(sum(-2, -3) == -5_i);
     };
