5faa4f146aa0c6c73296f0a783cc9b204a5d7503
Duta-Sebastian <sebiduta2004@gmail.com>
2024-12-12 00:07:26 +0200

    Rebased main

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -2,7 +2,6 @@ option(WARNINGS_AS_ERRORS "Treat warnings as errors" OFF)
 option(USE_ASAN "Use Address Sanitizer" OFF)
 option(USE_MSAN "Use Memory Sanitizer" OFF)
 option(CMAKE_COLOR_DIAGNOSTICS "Enable color diagnostics" ON)
-
 # update name in .github/workflows/cmake.yml:27 when changing "bin" name here
 set(DESTINATION_DIR "bin")
 
@@ -15,5 +14,3 @@ endif()
 set(is_debug "$<CONFIG:Debug>")
 set(is_rel_with_deb "$<CONFIG:RelWithDebInfo>")
 set(debug_mode "$<OR:${is_debug},${is_rel_with_deb}>")
-
-add_definitions(-DBOOST_UT_DISABLE_MODULE)
