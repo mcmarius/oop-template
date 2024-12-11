88c0ea5af442297a8f6015989feb40d0f72fd5bf
Duta-Sebastian <sebiduta2004@gmail.com>
2024-12-12 00:28:23 +0200

    Testare fara teste

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -2,6 +2,8 @@ option(WARNINGS_AS_ERRORS "Treat warnings as errors" OFF)
 option(USE_ASAN "Use Address Sanitizer" OFF)
 option(USE_MSAN "Use Memory Sanitizer" OFF)
 option(CMAKE_COLOR_DIAGNOSTICS "Enable color diagnostics" ON)
+option(RUN_TESTS "Builds and runs the test suites" OFF)
+
 # update name in .github/workflows/cmake.yml:27 when changing "bin" name here
 set(DESTINATION_DIR "bin")
 
@@ -14,3 +16,5 @@ endif()
 set(is_debug "$<CONFIG:Debug>")
 set(is_rel_with_deb "$<CONFIG:RelWithDebInfo>")
 set(debug_mode "$<OR:${is_debug},${is_rel_with_deb}>")
+
+add_definitions(-DBOOST_UT_DISABLE_MODULE)
