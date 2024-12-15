74d14fae5634b190f13108966b3148e9685836cc
bogdan <trifan.bogdan09@yahoo.com>
2024-12-15 21:42:11 +0200

    exemplu cu resurse locale

---
+++ ResourceManager.cpp
@@ -0,0 +1,65 @@
+//
+// Created by bgd on 12/15/2024.
+//
+
+#include "ResourceManager.hpp"
+
+ResourceManager& ResourceManager::Instance()
+{
+    static ResourceManager instance;
+    return instance;
+}
+
+ResourceManager::ResourceManager()
+{
+    loadTexture("images", "a380_emirates.png");
+
+    loadFont("fonts", "FiraSans-Regular.ttf");
+}
+
+void ResourceManager::loadTexture(const std::string& path, const std::string& texture_name)
+{
+    sf::Texture texture;
+
+    if(!texture.loadFromFile(path + '/' + texture_name))
+    {
+        throw std::runtime_error("Textura " + texture_name + " nu a putut fi incarcata.");
+    }
+
+    m_textures[texture_name] = texture;
+}
+
+void ResourceManager::loadFont(const std::string& path, const std::string& font_name)
+{
+    sf::Font font;
+
+    if(!font.loadFromFile(path + '/' + font_name))
+    {
+        throw std::runtime_error("Textura " + font_name + " nu a putut fi incarcata.");
+    }
+
+    m_fonts[font_name] = font;
+}
+
+
+sf::Texture& ResourceManager::getTexture(const std::string& texture_name)
+{
+    // Înainte de a returna o textură trebuie să verificăm dacă acea textură există.
+    if(m_textures.find(texture_name) == m_textures.end())
+    {
+        throw std::runtime_error("Textura " + texture_name + " nu a fost gasita.");
+    }
+
+    return m_textures[texture_name];
+}
+
+sf::Font& ResourceManager::getFont(const std::string& font_name)
+{
+    // Înainte de a returna un font trebuie să verificăm dacă acel font există.
+    if(m_fonts.find(font_name) == m_fonts.end())
+    {
+        throw std::runtime_error("Font-ul " + font_name + " nu a fost gasit.");
+    }
+
+    return m_fonts[font_name];
+}
