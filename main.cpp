cc47b269ffaecadb0333df8094ddc859bb053217
bogdan <trifan.bogdan09@yahoo.com>
2024-12-15 21:42:11 +0200

    exemplu cu resurse locale

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 #include <Helper.h>
 
+#include "ResourceManager.hpp"
+
 //////////////////////////////////////////////////////////////////////
 /// This class is used to test that the memory leak checks work as expected even when using a GUI
 class SomeClass {
@@ -19,52 +21,16 @@ SomeClass *getC() {
 }
 //////////////////////////////////////////////////////////////////////
 
+void PersonalizeazaAvion(sf::Sprite& avion)
+{
+    avion.setScale(0.3f, 0.3f);
+    avion.setPosition(400, 350);
+
+    sf::FloatRect bounds = avion.getGlobalBounds();
+    avion.setOrigin(bounds.left + bounds.width / 2, bounds.top + bounds.height / 2);
+}
 
 int main() {
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
     ///////////////////////////////////////////////////////////////////////////
     ///                Exemplu de utilizare cod generat                     ///
     ///////////////////////////////////////////////////////////////////////////
@@ -85,29 +51,72 @@ int main() {
     ///////////////////////////////////////////////////////////////////////////
     /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
     /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
+    // window.setVerticalSyncEnabled(true);                                    ///
+    window.setFramerateLimit(60);                                       ///
     ///////////////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////
+    /// Pentru a putea folosi resurse locale în cadrul programului trebuie modificat `CMakeLists.txt`.
+    /// Deschide `CMakeLists.txt` la linia 101.
+    ///
+    /// După pasul anterior, putem folosi resursele locale folosind sintaxa `ResourceManager::Instance().get*`, unde
+    /// * poate fi Texture, Font, Sound sau orice alt tip de resursă de care avem nevoie.
+    ////////////////////////////////////////////////////////
+
+    sf::Texture a380_texture;
+    try {
+        a380_texture = ResourceManager::Instance().getTexture("a380_emirates.png");
+    } catch(std::exception& exp) {
+        std::cout << exp.what() << std::endl;
+        return 0;
+    }
+
+    // Decomentează exemplul de mai jos pentru a vedea ce se întâmplă atunci când încerci să primești o textură inexistentă.
+    /*
+    sf::Texture dummyTexture;
+    try {
+        dummyTexture = ResourceManager::Instance().getTexture("fizzbuzz.png");
+    } catch(std::exception& exp) {
+        std::cout << exp.what();
+        return 0;
+    }
+    */
+
+    sf::Sprite a380;
+    a380.setTexture(a380_texture);
+
+    PersonalizeazaAvion(a380); // Este o funcție care modifică dimensiunea avionului și îi pune originea în centru.
+
+
+    sf::Text tip_avion;
+    try {
+        tip_avion.setFont(ResourceManager::Instance().getFont("FiraSans-Regular.ttf"));
+    } catch(std::exception& exp) {
+        std::cout << exp.what();
+        return 0;
+    }
+
+    tip_avion.setString("A380 Emirates");
 
     while(window.isOpen()) {
         bool shouldExit = false;
         sf::Event e{};
         while(window.pollEvent(e)) {
             switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
-                break;
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::Resized:
+                    std::cout << "New width: " << window.getSize().x << '\n'
+                              << "New height: " << window.getSize().y << '\n';
+                    break;
+                case sf::Event::KeyPressed:
+                    std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
+                    if(e.key.code == sf::Keyboard::Escape)
+                        shouldExit = true;
+                    break;
+                default:
+                    break;
             }
         }
         if(shouldExit) {
@@ -115,9 +124,15 @@ int main() {
             break;
         }
         using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
+        std::this_thread::sleep_for(150ms);
+
+        a380.setRotation(a380.getRotation() + 1);
 
         window.clear();
+
+        window.draw(a380); // Afișăm avionul încărcat pe ecran
+        window.draw(tip_avion); // Afișăm tipul avionului
+
         window.display();
     }
     return 0;
