2811fd004e331e61b7f89df22ce236c2cedce0f2
Duta-Sebastian <sebiduta2004@gmail.com>
2024-12-21 18:48:09 +0200

    Adaugat in readme

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -14,10 +14,10 @@ endif()
 # custom compiler flags
 message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
 if(MSVC)
-    target_compile_options(pqxx PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
+    target_compile_options(pqxx PRIVATE /w /utf-8 /MP)
     target_compile_options(${MAIN_EXECUTABLE_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
 else()
-    target_compile_options(pqxx PRIVATE -Wall -Wextra -pedantic -Wno-pre-c++2b-compat)
+    target_compile_options(pqxx PRIVATE -w)
     target_compile_options(${MAIN_EXECUTABLE_NAME} PRIVATE -Wall -Wextra -pedantic -Wno-pre-c++2b-compat)
 endif()
 
