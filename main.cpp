b296326dcf09499a0c664e5381606f7e00b77037
Duta-Sebastian <sebiduta2004@gmail.com>
2024-12-21 18:27:27 +0200

    Fix bug ubuntu

--- main.cpp
+++ main.cpp
@@ -56,6 +56,6 @@ int main() {
     ///////////////////////////////////////////////////////////////////////////
     ///                Exemplu de utilizare cod pqxx                        ///
     ///////////////////////////////////////////////////////////////////////////
-    Database &database = Database::getDatabaseInstance();
+    [[maybe_unused]] Database &database = Database::getDatabaseInstance();
     return 0;
 }
