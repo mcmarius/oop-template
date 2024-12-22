220ceb8c48695724b1b5512641c6493a20666f62
Duta-Sebastian <sebiduta2004@gmail.com>
2024-12-22 18:56:11 +0200

    Adaugat comentarii in main

--- main.cpp
+++ main.cpp
@@ -48,11 +48,12 @@ int main() {
     ///     fis >> v2[i];
     ///
     ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod pqxx                        ///
+    ///                Exemplu de utilizare pqxx                            ///
     ///////////////////////////////////////////////////////////////////////////
-    std::cout<< std::flush;
     Database &database = Database::getDatabaseInstance();
+    std::cout<<"\nBaza de date a fost inițializată";
     database.addUser();
+    std::cout<<"\nA fost adaugat un utilizator:";
     auto result = database.showUsers();
     for (const auto& row : result)
     {
