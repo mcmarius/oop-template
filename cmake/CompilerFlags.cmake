500208cbdb502bd174a4f0590c7126be49f112ed
Duta-Sebastian <48097905+Duta-Sebastian@users.noreply.github.com>
2025-01-13 00:25:29 +0200

    Refactor compiler flags (#40)
    * Parametrizat target in CompilerFlags + Mutat toate include-urile din CMakeLists.txt la un loc
    
    * Adaugat un comentariu pentru functie + test warnings as errors
    
    * Scos test pentru warnings as errors
    
    Rezolvă o parte din #39

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,27 +1,35 @@
-# target definitions
-
-if(GITHUB_ACTIONS)
-    message("NOTE: GITHUB_ACTIONS defined")
-    target_compile_definitions(${MAIN_EXECUTABLE_NAME} PRIVATE GITHUB_ACTIONS)
-endif()
-
-###############################################################################
-
-if(PROJECT_WARNINGS_AS_ERRORS)
-    set_property(TARGET ${MAIN_EXECUTABLE_NAME} PROPERTY COMPILE_WARNING_AS_ERROR ON)
-endif()
-
-# custom compiler flags
-message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
-if(MSVC)
-    target_compile_options(${MAIN_EXECUTABLE_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
-else()
-    target_compile_options(${MAIN_EXECUTABLE_NAME} PRIVATE -Wall -Wextra -pedantic)
-endif()
-
-###############################################################################
-
-# sanitizers
 include(cmake/CustomStdlibAndSanitizers.cmake)
 
-set_custom_stdlib_and_sanitizers(${MAIN_EXECUTABLE_NAME} true)
+# target definitions
+
+function(set_compiler_flags)
+    set(multiValueArgs TARGET_NAMES)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "" "${multiValueArgs}")
+
+    # iterate over all specified targets
+    foreach (TARGET_NAME IN LISTS ARG_TARGET_NAMES)
+        if(GITHUB_ACTIONS)
+            message("NOTE: GITHUB_ACTIONS defined")
+            target_compile_definitions(${TARGET_NAME} PRIVATE GITHUB_ACTIONS)
+        endif()
+
+        ###############################################################################
+
+        if(PROJECT_WARNINGS_AS_ERRORS)
+            set_property(TARGET ${TARGET_NAME} PROPERTY COMPILE_WARNING_AS_ERROR ON)
+        endif()
+
+        # custom compiler flags
+        message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
+        if(MSVC)
+            target_compile_options(${TARGET_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
+        else()
+            target_compile_options(${TARGET_NAME} PRIVATE -Wall -Wextra -pedantic)
+        endif()
+
+        ###############################################################################
+
+        # sanitizers
+        set_custom_stdlib_and_sanitizers(${TARGET_NAME} true)
+    endforeach ()
+endfunction()
