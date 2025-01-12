amyxkx/BakeryOOP

Disable modules for boost-ut in CMAKE pt MSVC

cmake/CompilerFlags.cmake
@@ -14,7 +14,7 @@ endif()
 # custom compiler flags
 message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
 if(MSVC)
-    target_compile_options(${PROJECT_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
+    target_compile_options(${PROJECT_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP /d1no_modules)
 else()
     target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -pedantic)
 endif()

mcmarius/oop-template

cmake/CompilerFlags.cmake
@@ -22,7 +22,7 @@ function(set_compiler_flags)
         # custom compiler flags
         message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
         if(MSVC)
-            target_compile_options(${TARGET_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
+            target_compile_options(${TARGET_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP /d1no_modules)
         else()
             target_compile_options(${TARGET_NAME} PRIVATE -Wall -Wextra -pedantic)
         endif()