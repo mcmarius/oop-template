8992e3312aabf613f7feebb287f3e9bcb93d9274
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-02-25 01:21:53 +0200

    Refactor example
    - better logs
    - shutdown server using signals
    - remove old sleep-based shutdown logic
    - more comments
    - add 404 route
    - mention ext lib modifications in README

--- main-client.cpp
+++ main-client.cpp
@@ -1,15 +1,21 @@
-#include <cstdlib>
-// #define CPPHTTPLIB_OPENSSL_SUPPORT
+// #include <cstdlib>
 #include <httplib.h>
 
 int main() {
-    // HTTP
     std::string url = "http://127.0.0.1:8080";
-    if(const char* url_chr = std::getenv("SERVER_URL"))
-        url = url_chr;
+    // configure server url through env variables
+    // to avoid recompiling when changing host or port
+    // if(const char* url_chr = std::getenv("SERVER_URL")) {
+    //     url = url_chr;
+    // }
+    // client object
     httplib::Client cli(url);
 
+    // send request
     auto res = cli.Get("/hi");
-    std::cout << "Got status " << res->status << "\n";
-    std::cout << "Got body " << res->body << "\n";
+    // process response
+    std::cout << "[client] Got status " << res->status << "\n";
+    std::cout << "[client] Got body " << res->body << "\n";
+    // of course, the client can send/process multiple requests
+    // this is just a minimal example
 }
