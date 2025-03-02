eff08ecee0711f679e0cf4baa86cc5fa4499362e
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-03-02 17:11:14 +0200

    More sleep and suppressions for valgrind
    Also add a message so it is clearer in the CI logs
    that the code actually works

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -5,13 +5,13 @@ INPUT_FILENAME=${INPUT_FILENAME:-tastatura.txt}
 ZIP_NAME=${ZIP_NAME:-install_dir/bin}
 EXECUTABLE_NAME=${EXECUTABLE_NAME:-oop}
 
-#           --gen-suppressions=all \
 tr -d '\r' < "${INPUT_FILENAME}" | \
   valgrind --leak-check=full \
+           --gen-suppressions=all \
            --show-leak-kinds=all \
            --leak-resolution=med \
            --track-origins=yes \
            --vgdb=no \
            --suppressions=./scripts/valgrind-suppressions.supp \
            --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
-bash ./scripts/run_test.sh 10 1 2
+bash ./scripts/run_test.sh 13 1 2
