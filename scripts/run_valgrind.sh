f059a1c54c0420beb5e6c48f93d2e2295190445d
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-03-02 19:14:45 +0200

    Adăugare instrucțiuni lipsă
    - script valgrind, suppressions
    - cache deps

---
+++ scripts/run_valgrind.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+INPUT_FILENAME=${INPUT_FILENAME:-tastatura.txt}
+ZIP_NAME=${ZIP_NAME:-install_dir/bin}
+EXECUTABLE_NAME=${EXECUTABLE_NAME:-oop}
+
+tr -d '\r' < "${INPUT_FILENAME}" | \
+  valgrind --leak-check=full \
+           --gen-suppressions=all \
+           --show-leak-kinds=all \
+           --leak-resolution=med \
+           --track-origins=yes \
+           --vgdb=no \
+           --suppressions=./scripts/valgrind-suppressions.supp \
+           --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}"
