5e3ed4f44ebb609249f6650c3a1dc39cd264fe48
bgd <trifan.bogdan09@yahoo.com>
2025-03-19 23:55:27 +0200

    uncomment window.draw

--- main.cpp
+++ main.cpp
@@ -129,8 +129,8 @@ int main() {
 
         window.clear();
 
-        //window.draw(avion); // Afișăm avionul încărcat pe ecran
-        //window.draw(tip_avion); // Afișăm tipul avionului
+        window.draw(avion); // Afișăm avionul încărcat pe ecran
+        window.draw(tip_avion); // Afișăm tipul avionului
 
         window.display();
     }
