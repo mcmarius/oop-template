30c8741f116bff077d62cc2e2bc77cf019f87602
bgd <trifan.bogdan09@yahoo.com>
2025-03-19 23:11:08 +0200

    elimina flag-uri valgrind (test)

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -11,6 +11,5 @@ tr -d '\r' < "${INPUT_FILENAME}" | \
            --leak-resolution=med \
            --track-origins=yes \
            --vgdb=no \
-           --suppressions=./scripts/valgrind-suppressions.supp \
            --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
 bash ./scripts/run_test.sh 13 1 2
