a0e61baf35f662565c62629f928cb133b544e7f5
bgd <trifan.bogdan09@yahoo.com>
2025-03-19 23:09:07 +0200

    elimina flag-uri valgrind (test)

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -11,6 +11,13 @@ ResourceManager& ResourceManager::Instance()
     return instance;
 }
 
+ResourceManager::ResourceManager()
+{
+    loadTexture("images", "airplane.png");
+
+    loadFont("fonts", "FiraSans-Regular.ttf");
+}
+
 void ResourceManager::loadTexture(const std::string& path, const std::string& texture_name)
 {
     sf::Texture texture;
