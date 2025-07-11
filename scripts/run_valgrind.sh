75c4932761a751f7d4dd833175e1a8d7e2917dc3
bgd <trifan.bogdan09@yahoo.com>
2025-07-11 20:00:20 +0300

    Adaugat FTXUI in CMakeLists.txt Adaugat un exemplu cu FTXUI
    changed headers
    
    set cmake_cxx_flags
    
    change - to /
    
    set flags
    
    add fsanitize=address
    
    disable msan
    
    opreste executia dupa 5 secunde
    
    remove unused header
    
    suppress tcsetattr
    
    create suppression file
    
    update suppression file name in action.yml
    
    update ftxui tcsetattr suppression
    
    update suppressions
    
    --gen-suppressions=all
    
    update suppressions

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -19,6 +19,7 @@ run_valgrind() {
              --show-leak-kinds=all \
              --track-origins=yes \
              --error-exitcode=1 \
+             --suppressions=./scripts/valgrind-suppressions.supp \
              ./"${BIN_DIR}"/"${EXECUTABLE_NAME}"
 }
 
