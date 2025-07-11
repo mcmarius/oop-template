728a675534481f7bee270de9950fc6f3572098c8
bgd <trifan.bogdan09@yahoo.com>
2025-07-11 21:09:50 +0300

    changed headers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "ftxui/component/component.hpp"       // for Slider, Renderer, Vertical
-#include "ftxui/component/component_base.hpp"  // for ComponentBase
-#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for separator, operator|, Element, size, text, vbox, xflex, bgcolor, hbox, GREATER_THAN, WIDTH, border, HEIGHT, LESS_THAN
-#include "ftxui/screen/color.hpp"  // for Color
+#include <ftxui/component/component.hpp>       // for Slider, Renderer, Vertical
+#include <ftxui/component/component_base.hpp>  // for ComponentBase
+#include <ftxui/component/screen_interactive.hpp>  // for ScreenInteractive
+#include <ftxui/dom/elements.hpp>  // for separator, operator|, Element, size, text, vbox, xflex, bgcolor, hbox, GREATER_THAN, WIDTH, border, HEIGHT, LESS_THAN
+#include <ftxui/screen/color.hpp>  // for Color
 
 #include <Helper.h>
 
