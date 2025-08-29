29ced47016622eb422e1a336fb2a8e137f0af893
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-08-29 17:58:58 +0300

    Update dependency jtv/libpqxx to v7.10.1 (baze-de-date/pqxx) (#76)
    * Replace all pqxx exec_* calls with exec
    
    Update dependency jtv/libpqxx to v7.10.3 (#107)
    
    Update dependency jtv/libpqxx to v7.10.5 (#142)

--- src/database/Database.cpp
+++ src/database/Database.cpp
@@ -51,17 +51,17 @@ void Database::dropTables(){
 void Database::addUser(const std::string& name, const std::string& password) {
     pqxx::work transaction(*connection);
     std::string hashQuery = "SELECT crypt($1, gen_salt('bf')) AS hashed_password;";
-    pqxx::result hashResult = transaction.exec_params(hashQuery,password);
+    pqxx::result hashResult = transaction.exec(hashQuery, pqxx::params{password});
     std::string hashedPassword = hashResult[0]["hashed_password"].c_str();
     std::string insertUserQuery = "INSERT INTO USERS (name, password) VALUES ($1, $2);";
-    transaction.exec_params(insertUserQuery, name, hashedPassword);
+    transaction.exec(insertUserQuery, pqxx::params{name, hashedPassword});
     transaction.commit();
 }
 
 std::vector<std::tuple<std::string,std::string>> Database::showAllUsers() {
     pqxx::work transaction(*connection);
     std::vector<std::tuple<std::string,std::string>> result;
-    const auto queryResult = transaction.exec_params("SELECT * FROM USERS");
+    const auto queryResult = transaction.exec("SELECT * FROM USERS");
     for (const auto &row : queryResult)
     {
         result.emplace_back(row[1].as<std::string>(),row[2].as<std::string>());
@@ -72,7 +72,7 @@ std::vector<std::tuple<std::string,std::string>> Database::showAllUsers() {
 std::tuple<std::string,std::string> Database::showUser(const std::string& name) {
     pqxx::work transaction(*connection);
     std::vector<std::tuple<std::string,std::string>> result;
-    const auto queryResult = transaction.exec_params("SELECT * FROM USERS WHERE name = $1", name);
+    const auto queryResult = transaction.exec("SELECT * FROM USERS WHERE name = $1", pqxx::params{name});
     return {queryResult[0][1].as<std::string>(),queryResult[0][2].as<std::string>()};
 }
 
@@ -81,20 +81,20 @@ void Database::deleteUser(const std::string& name) {
     std::string deleteUserQuery = R"(
         DELETE FROM USERS WHERE name = $1;
     )";
-    transaction.exec_params(deleteUserQuery,name);
+    transaction.exec(deleteUserQuery, pqxx::params{name});
     transaction.commit();
 }
 
 void Database::updateUserPassword(const std::string& name, const std::string& newPassword) {
     pqxx::work transaction(*connection);
     std::string hashQuery = "SELECT crypt($1, gen_salt('bf')) AS hashed_password;";
-    pqxx::result hashResult = transaction.exec_params(hashQuery,newPassword);
+    pqxx::result hashResult = transaction.exec(hashQuery, pqxx::params{newPassword});
     std::string hashedPassword = hashResult[0]["hashed_password"].c_str();
     std::string updateUserPasswordQuery = R"(
         UPDATE USERS
         SET password = $1
         WHERE name = $2;)";
-    transaction.exec_params(updateUserPasswordQuery,hashedPassword, name);
+    transaction.exec(updateUserPasswordQuery, pqxx::params{hashedPassword, name});
     transaction.commit();
 }
 
