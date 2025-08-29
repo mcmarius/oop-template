feb6851b6cc1040a2d4618811ac317e1c46b762b
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-08-29 13:43:48 +0300

    Update dependency danmar/cppcheck to v2.18.1 (#82)

--- scripts/build_cppcheck.sh
+++ scripts/build_cppcheck.sh
@@ -2,7 +2,7 @@
 
 # default values
 # renovate: datasource=github-tags depName=danmar/cppcheck versioning=loose
-CPPCHECK_VER=2.14.2   # NOTE: consider updating this value in .github/workflows/cmake.yml:47 when changing this value
+CPPCHECK_VER=2.18.1   # NOTE: consider updating this value in .github/workflows/cmake.yml:47 when changing this value
 CMAKE_BUILD_DIR=build
 CMAKE_OPTS=()         # example for CLI: -o "-DCMAKE_INSTALL_PREFIX=~/.local/ -DFILESDIR=~/.local/share/Cppcheck"
 
