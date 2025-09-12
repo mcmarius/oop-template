0767e1408b2127e19a920b0b456d5b5a4f62f958
Bogdanctx <trifan.bogdan09@yahoo.com>
2025-09-12 15:48:54 +0300

    Instaleaza si ruleaza binarul pentru cppcheck pe windows

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,42 +1,23 @@
 #!/usr/bin/bash
 
-AUTO_INSTALL=${AUTO_INSTALL:-0}
-OS=$(uname)
-
-
-while getopts ":i:" opt; do
-  case "${opt}" in
-    i) AUTO_INSTALL=1
-    ;;
-    *) printf "Unknown option %s; available options: \n\
-        -i (install if not found)\n" "${opt}"
-      exit 1
-    ;;
-  esac
-done
+# AUTO_INSTALL=${AUTO_INSTALL:-0}
+# while getopts ":i" opt; do
+#   case "${opt}" in
+#     i) AUTO_INSTALL=1
+#     ;;
+#     *) printf "Unknown option %s; available options: \n\
+#         -i (install if cppcheck not found)\n" "${opt}"
+#       exit 1
+#     ;;
+#   esac
+# done
 
 
 if ! command -v cppcheck >/dev/null 2>&1; then # daca cppcheck nu exista in path
-  if [[ ${AUTO_INSTALL} -eq 1 ]]; then # daca e setat AUTO_INSTALL
-    echo "[INFO] cppcheck not found. Installing..."
-    
-    if [[ "${OS}" == "Linux" ]]; then # instalez cppcheck pe linux
-      INSTALL_DIR="${HOME}/.local"
-      ./scripts/build_cppcheck.sh -o "-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DFILESDIR=${INSTALL_DIR}/share/cppcheck"
-      export PATH="${INSTALL_DIR}/bin:${PATH}"
-    elif [[ "${OS}" == MINGW* || "${OS}" == MSYS* || "${OS}" == CYGWIN* ]]; then # instalez cppcheck pe windows 
-      INSTALL_DIR="$(cygpath -y "${APPDATA}")/cppcheck"
-      mkdir -p "${INSTALL_DIR}"
-      ./scripts/build_cppcheck.sh -o "-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
-      export PATH="${INSTALL_DIR}/bin:${PATH}"
-    fi
-  
-  else
-    echo "[INFO] cppcheck not found (AUTO_INSTALL = 0)"
-    exit 1
-  fi
+  echo "[INFO] Cppcheck nu este instalat. Se va descarca un installer pentru a instala Cppcheck."
+  ./scripts/build_cppcheck.sh
 else
-  echo "[INFO] cppcheck found."
+  echo "[INFO] Cppcheck found."
 fi
 
 cppcheck --enable=all \
