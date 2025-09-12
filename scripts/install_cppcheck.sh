a4a9c13327ce5d493fedf0d4f4b1625e5300c500
Bogdanctx <trifan.bogdan09@yahoo.com>
2025-09-12 17:38:44 +0300

    Refactor cppcheck installation scripts and improve auto-install feature

---
+++ scripts/install_cppcheck.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# renovate: datasource=github-releases depName=danmar/cppcheck versioning=loose
+CPPCHECK_WINVER=2.18.0
+
+OS=$(uname)
+if [[ "${OS}" == MINGW* || "${OS}" == MSYS* || "${OS}" == CYGWIN* ]]; then
+    curl -L -o "cppcheck-${CPPCHECK_WINVER}-x64-Setup.msi" "https://github.com/danmar/cppcheck/releases/download/${CPPCHECK_WINVER}/cppcheck-${CPPCHECK_WINVER}-x64-Setup.msi"
+    powershell.exe -Command "Start-Process 'cppcheck-${CPPCHECK_WINVER}-x64-Setup.msi' -Wait"
+    echo "Dupa instalare, cppcheck va trebui adaugat in PATH."
+fi
