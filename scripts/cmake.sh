a5c241b2ed87de41dfdca99e74339ec4fac7b1a4
Bogdanctx <trifan.bogdan09@yahoo.com>
2025-09-19 18:26:02 +0300

    use local sfml; set priority: cmake vars > env vars

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -4,6 +4,7 @@ DEFAULT_BUILD_DIR="build"
 DEFAULT_BUILD_TYPE="Debug"
 DEFAULT_INSTALL_DIR="install_dir"
 
+# Pentru a folosi SFML local: ./scripts/cmake.sh configure -e "-DFETCHCONTENT_BASE_DIR=~/.local/sfml-deps -DFETCHCONTENT_SOURCE_DIR_SFML=~/.local/SFML"
 configure() {
     # cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
     #
